b11ea602fb233dc8724071da42014e274456f55f
Vitor Bastos <vbss-services@outlook.com>
2024-12-17 19:02:59 -0300

    feat: move provider types to types

--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,18 @@ export type Translation = {
   [key: string]: string;
 };
 
+export type TranslatorProviderType = {
+  children: React.ReactNode;
+  translations: Translation[];
+  defaultLanguage?: string;
+  persist?: boolean;
+  persistKey?: string;
+  autoDetectLanguage?: boolean
+};
+
 export type TranslatorContextType = {
   t: (key: string) => string;
   language: string;
+  languages: string[];
   setLanguage: (language: string) => void;
 };
