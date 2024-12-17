8a4459eda3499b6dbfcd32063d3edba027ca74fc
Vitor Bastos <vbss-services@outlook.com>
2024-12-17 19:03:32 -0300

    feat: add auto detect, persisted language and console warn

--- src/contexts/TranslatorProvider.tsx
+++ src/contexts/TranslatorProvider.tsx
@@ -1,28 +1,55 @@
 import { TranslatorContext } from "@/contexts/TranslatorContext";
-import { Translation } from "@/types";
-import { useState } from "react";
+import { TranslatorProviderType } from "@/types";
+import { useEffect, useState } from "react";
+
+const getBrowserLanguage = (fallback: string) => {
+  const lang = navigator?.language?.split('-')?.[0];
+  return lang || fallback;
+}
+
+const getPersistedLanguage = (key: string) => {
+  return localStorage.getItem(key);
+}
+
+const setPersistedLanguage  = (persistKey: string, lang: string) => {
+  localStorage.setItem(persistKey, lang);
+}
 
 export const TranslatorProvider = ({
   children,
-  defaultLanguage = 'en',
   translations,
-}: {
-  children: React.ReactNode;
-  defaultLanguage?: string;
-  translations: Translation[];
-}) => {
-  const [language, setLanguage] = useState<string>(defaultLanguage);
+  defaultLanguage = 'pt',
+  persist = false,
+  persistKey = 'language',
+  autoDetectLanguage = false
+}: TranslatorProviderType) => {
+  const persistedLanguage = persist ? getPersistedLanguage(persistKey) : undefined
+  const initialLanguage = autoDetectLanguage && !persistedLanguage ? getBrowserLanguage(defaultLanguage) : defaultLanguage;
+  const [language, setLanguage] = useState<string>(initialLanguage);
+
+  useEffect(() => {
+    if (persist) {
+      setPersistedLanguage(persistKey, language)
+    }
+  }, [language, persist, persistKey])
+
+  const languages = Object.keys(translations[0] || {})
 
   const t = (text: string): string => {
     const translation = translations.find((item) => {
       const values = Object.values(item)
-      return (values.some((value) => value === text))
+      return (values.some((value) => value.toLocaleLowerCase() === text.toLocaleLowerCase()))
     })
+
+    if (!translation?.[language]) {
+      console.warn(`[Translator Debug] Missing translation "${language}" for: '${text}'`);
+    }
+
     return translation ? translation[language] : text;
   };
 
   return (
-    <TranslatorContext.Provider value={{ t, language, setLanguage }}>
+    <TranslatorContext.Provider value={{ t, language, languages, setLanguage }}>
       {children}
     </TranslatorContext.Provider>
   );
